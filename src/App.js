cfe98ee85a609c53ce0dbb31ea6303f98dbaa9d6
girimohan <45201945+girimohan@users.noreply.github.com>
2024-11-16 18:01:25 +0200

    fix routing for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
